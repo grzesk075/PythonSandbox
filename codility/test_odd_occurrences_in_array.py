e339117ef9e54328d58b67d8983574e16ea9601e
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2019-07-06 19:59:11 +0200

    Correct odd occurrences in array codility task.

--- codility/test_odd_occurrences_in_array.py
+++ codility/test_odd_occurrences_in_array.py
@@ -8,9 +8,14 @@ class TestOddOccurrencesInArray(TestCase):
         self.assertEqual(solution([1, 7, 1, 7, 8, 8, 9, 2, 3, 2, 3]), 9)
         self.assertEqual(solution([1, 5, 1000000000, 1, 1000000000, 1, 1000000000, 5, 9, 1, 1000000000, 3, 6, 6, 3]), 9)
         self.assertEqual(solution([5]), 5)
+        self.assertEqual(solution([1, 1, 1]), 1)
+        self.assertEqual(solution([3, 3, 3, 3, 3]), 3)
+        self.assertEqual(solution([1, 3, 3, 3, 3, 3, 1]), 3)
         self.assertEqual(solution([1, 1000000000, 1000000000]), 1)
         self.assertEqual(solution([1, 1, 1000000000]), 1000000000)
         self.assertEqual(solution([1, 1, 1, 1, 1000000000]), 1000000000)
+        self.assertEqual(solution([1, 1, 1, 1, 1, 9, 9]), 1)
+        self.assertEqual(solution([1, 1, 1, 1, 1000000000, 1000000000, 1000000000]), 1000000000)
         self.assertEqual(solution([12] * 100000 + [1] * 100000 + [7] + [1000000000] * 700000 + [345] * (100000 - 2)), 7)
 
 
