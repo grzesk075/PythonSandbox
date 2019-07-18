6a4e5beeab680183ac7a74a8357a9f816a1811b2
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2019-07-18 19:24:50 +0200

    Frog jump refactoring.

--- codility/frog_jmp.py
+++ codility/frog_jmp.py
@@ -1,6 +1,6 @@
 #  O(1)
-def solution(X, Y, K):
+def solution(X, Y, D):
     distance = Y - X
-    jumps_floor = distance // K
-    jumps_rest = distance % K
+    jumps_floor = distance // D
+    jumps_rest = distance % D
     return jumps_floor if jumps_rest == 0 else jumps_floor + 1
