9fa879b4a15a971258c458bbfe55c7a899c95ce5
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2019-11-20 15:10:18 +0100

    Variable scope test extension.

--- basics/scopes.py
+++ basics/scopes.py
@@ -12,12 +12,24 @@ def scope_test():
 
     spam = "test spam"
     do_local()
-    print("After local assignment:", spam)
+    print("After local assignment:", spam)  # test spam
     do_nonlocal()
-    print("After nonlocal assignment:", spam)  # current scope override global scope
+    print("After nonlocal assignment:", spam)  # nonlocal spam
     do_global()
-    print("After global assignment:", spam)
+    print("After global assignment:", spam)  # nonlocal spam
+
+    # another use case
+    numbers = [1, 2, 3]
+    if True:
+        numbers = [4, 5, 6]
+        letters = ['a', 'b', 'c']  # This is visible in scope of whole function!
+
+    print('Numbers: ', numbers)  # Numbers:  [4, 5, 6]
+    print('Letters: ', letters)  # Letters:  ['a', 'b', 'c']
 
 
 scope_test()
-print("In global scope:", spam)
+print("In global scope:", spam)  # global spam
+
+if __name__ == '__main__':
+    print("In main function scope:", spam)  # global spam
